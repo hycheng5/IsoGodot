ac2808389bb8d6c1f0309d3a76a7a45805d16775
hycheng5 <henryx75@gmail.com>
2024-09-24 18:19:38 -0700

    added destroy timer for debug and debug option for click controller

--- character/scripts/click_controller.gd
+++ character/scripts/click_controller.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@export var debug: bool
 @export var SPEED = 300
 @onready var camera: Camera2D = %Camera2D
 var debug_scene = preload("res://debug/debug_point.tscn")
@@ -23,9 +23,10 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_pressed("select"):
 		clickPosition = get_global_mouse_position()
-		var debug_instance:Node2D = debug_scene.instantiate()
-		add_child(debug_instance)
-		debug_instance.position = (clickPosition - position)
+		if(debug):
+			var debug_instance:Node2D = debug_scene.instantiate()
+			add_child(debug_instance)
+			debug_instance.position = (clickPosition - position)
 		
 	if position.distance_to(clickPosition) > 40:
 		targetPosition = (clickPosition - position).normalized()
